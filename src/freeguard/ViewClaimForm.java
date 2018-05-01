4035935b18bfd8550eb4e11f6f7812595b2aca60
Matt Stillwell <stillwell006@gmail.com>
2018-05-01 13:51:21 -0400

    fixed-bug1

--- src/freeguard/ViewClaimForm.java
+++ src/freeguard/ViewClaimForm.java
@@ -23,15 +23,14 @@ public class ViewClaimForm extends JFrame
     String DB_URL = db.JDBC_DB_URL;
 
     //  Database credentials
-     String USER = db.username;
-     String PASS = db.password;
-    
-    protected static final String String = null;
-    static final int ROWNUM = 2;
+    String USER = db.username;
+    String PASS = db.password;
+
+    static final int COLUMN_NUM = 2;
 
     public ViewClaimForm(String username)
     {
-        
+
         resultFrame = new JFrame();      //create object of JPanel
         resultFrame.setTitle("View Claims");                                                                                                //set title
         resultFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);                        //set close operation                                                                          
@@ -39,10 +38,9 @@ public class ViewClaimForm extends JFrame
 
         resultFrame.setAlwaysOnTop(true);
 
-
         //string builder used to format and store the resulting tables
-        int columnNum = 0;
-        
+        int rowNum = 0;
+
         JLabel lblTitle = new JLabel("Claims");
         lblTitle.setBounds(70, 25, 86, 14);
 
@@ -53,63 +51,65 @@ public class ViewClaimForm extends JFrame
         System.out.println("Connecting to a selected database...");
         System.out.println(SQuery);
 
-        Wrapper conn = null;                                                                //Create wrapper object and define it null
+        Wrapper snoopdog = null;                                                                //Create wrapper object and define it null
         try                                                                                                                                                          //try block
         {
 
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);                            // Open a connection
+            snoopdog = DriverManager.getConnection(DB_URL, USER, PASS);                            // Open a connection
             System.out.println("Connected database successfully...");
-            Statement stmt = ((Connection) conn).createStatement();
+            Statement stmt = ((Connection) snoopdog).createStatement();
             ResultSet rs = stmt.executeQuery(SQuery);
             ResultSetMetaData rsmd = rs.getMetaData();
-            columnNum = rsmd.getColumnCount();                                                          //Get number of Columns
-            Object [] [] cellData = new Object [columnNum] [ROWNUM];
-            String [] columnNames = new String [columnNum];
-            for (int i = 0; i< ROWNUM; i++)
-            {
-                 columnNames [i] = rsmd.getColumnName(i+1);
-            }
+
+            Statement stmt2 = ((Connection) snoopdog).createStatement();
+            ResultSet goofy = stmt2.executeQuery(SQuery);
+            int size = 0;
+            goofy.next();
+            while (goofy.next())
+                size++;
+
+            rowNum = size;                                                          //Get number of Columns
+            System.out.println("DFDSF " + rowNum);
+            Object[][] cellData = new Object[rowNum][COLUMN_NUM];
+            String[] columnNames = new String[COLUMN_NUM];
+
+            for (int i = 0; i < COLUMN_NUM; i++)
+                columnNames[i] = rsmd.getColumnName(i + 1);
+
             int i = 0;
             rs.next();
             while (rs.next())
             {
-                for (int j = 0; j < ROWNUM; j++)   //for loop nested in while loop to retrieve the results table
+                for (int j = 0; j < COLUMN_NUM; j++)   //for loop nested in while loop to retrieve the results table
                 {
-                    cellData [i] [j] = rs.getString(j+1);
+
+                    System.out.println(rs.getString(j + 1));
+                    cellData[i][j] = rs.getString(j + 1);
                 }
                 i++;
             }
             // JOptionPane.showMessageDialog(null, SMessage, "Message", JOptionPane.PLAIN_MESSAGE);
             //close connection
-            ((java.sql.Connection) conn).close();
-            resultTable =  new JTable (cellData, columnNames);
+            ((java.sql.Connection) snoopdog).close();
+
+            for (Object o : cellData)
+                System.out.println(o);
+
+            for (String s : columnNames)
+                System.out.println(s);
+
+            resultTable = new JTable(cellData, columnNames);
             resultFrame.add(new JScrollPane(resultTable));
             resultFrame.setVisible(true);
-        }
-        catch (SQLException se)
+        } catch (SQLException se)
         {
             //handle errors for JDBC
             se.printStackTrace();
-        }
-        catch (Exception a) //catch block
+        } catch (Exception a) //catch block
         {
             a.printStackTrace();
         }
 
     }
 
-//    public static void main(String[] args)
-//    {
-//        try  //try block
-//        {
-//            //create NewUser frame object
-//            Login frame = new Login();
-//            //set NewUser frame visible
-//            frame.setVisible(true);
-//        }
-//        catch (Exception e) //catch block
-//        {
-//            e.printStackTrace();
-//        }
-//    }
 }
